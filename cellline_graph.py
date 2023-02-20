aba97cc78b88ea70b09d29c92cb801bc3e3c1c03
Bonil Koo <bikoo95@snu.ac.kr>
2023-02-20 14:29:47 +0900

    update script for preprocessing cell line data

--- cellline_graph.py
+++ cellline_graph.py
@@ -1,101 +1,100 @@
 import numpy as np
 import pandas as pd
 import os
-import csv
-import scipy
 import torch
-import torch.nn as nn
-from torch_geometric.data import Data, Batch
-from torch_geometric.nn import graclus, max_pool
+from torch_geometric.data import Data
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from scipy import sparse
 import pickle
-from tqdm import trange, tqdm
+from tqdm import tqdm
+import argparse
 
 
-def ensp_to_hugo_map():
-    with open(rpath+'Data/CELL/9606.protein.info.v11.5.txt') as csv_file:
-        next(csv_file)  # Skip first line
-        csv_reader = csv.reader(csv_file, delimiter='\t')
-        ensp_map = {row[0]: row[1] for row in csv_reader if row[0] != ""}
+def parse_args():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--path_data', default='./Data/Cell')
+    parser.add_argument('--kegg', default='./Data/Cell/34pathway_score990.pkl', help='A dictionary where the key is the pathway and the value is the genes belonging to the pathway.')
+    parser.add_argument('--type', default='disjoint', choices=['joint', 'disjoint'])
+    parser.add_argument('--exp', default='./Data/Cell/CCLE_2369_EXP.csv', help='A csv file where row is cell line, column is gene, and value is the gene expression value')
+    parser.add_argument('--ppi', default='./Data/Cell/CCLE_2369_PPI_990.csv', help='A csv file where row and column are gene, and value is 1 if there is an edge and 0 otherwise')
+    
+    return parser.parse_args()
 
-    return ensp_map
+def save_cell_graph(args):
+    with open(args.kegg, 'rb') as file:
+        kegg = pickle.load(file)
+        
+    # os.makedirs(save_path)
+    exp = pd.read_csv(os.path.join(args.exp), index_col=0)
+    index = exp.index
+    columns = exp.columns
+
+    scaler = StandardScaler()
+    exp = scaler.fit_transform(exp)
+    # cn = scaler.fit_transform(cn)
+    with open(os.path.join(args.path_data, 'scaler.pkl'), 'wb') as file:
+        pickle.dump(scaler, file)
+
+    imp_mean = SimpleImputer()
+    exp = imp_mean.fit_transform(exp)
+    with open(os.path.join(args.path_data, 'imp_mean.pkl'), 'wb') as file:
+        pickle.dump(imp_mean, file)
+
+    exp = pd.DataFrame(exp, index=index, columns=columns)
+    # cn = pd.DataFrame(cn, index=index, columns=columns)
+    # mu = pd.DataFrame(mu, index=index, columns=columns)
+    cell_names = exp.index
+
+    cell_dict = {}
+
+    for i in tqdm((cell_names)):
+
+        # joint graph (without pathway)
+        if type == 'joint':
+            gene_list = exp.columns.to_list()
+            gene_list = set()
+            for pw in kegg:
+                for gene in kegg[pw]:
+                    if gene in exp.columns.to_list():
+                        gene_list.add(gene)
+            gene_list = list(gene_list)
+            cell_dict[i] = Data(x=torch.tensor([exp.loc[i, gene_list]], dtype=torch.float).T)
+            # cell_dict[i] = Data(x=torch.tensor([cn.loc[i]], dtype=torch.float).T)
+            # cell_dict[i] = Data(x=torch.tensor([mu.loc[i]], dtype=torch.float).T)
+            # cell_dict[i] = Data(x=torch.tensor([exp.loc[i], cn.loc[i], mu.loc[i]], dtype=torch.float).T)
+            # cell_dict[i] = Data(x=torch.tensor([exp.loc[i], cn.loc[i], mu.loc[i], me.loc[i]], dtype=torch.float).T)
+            # cell_dict[i] = [np.array(exp.loc[i], dtype=np.float32), np.array(cn.loc[i], dtype=np.float32), np.array(mu.loc[i], dtype=np.float32)] # MLP용 코드
+
+        # disjoint graph (with pathway)
+        else:
+            genes = exp.columns.to_list()
+            x_mask = []
+            x = []
+            gene_list = {}
+            for p, pw in enumerate(list(kegg)):
+                gene_list[pw] = []
+                for gene in kegg[pw]:
+                    if gene in genes:
+                        gene_list[pw].append(gene)
+                        x_mask.append(p)
+                x.append(exp.loc[i, gene_list[pw]])
+            x = pd.concat(x)
+            cell_dict[i] = Data(x=torch.tensor([x], dtype=torch.float).T, x_mask=torch.tensor(x_mask, dtype=torch.int))
+
+    # print(cell_dict)
+    with open(os.path.join(args.path_data, f'cell_feature_std_{args.type}.pkl'), 'wb') as file:
+              pickle.dump(cell_dict, file)
+    print("finish saving cell data!")
+    return gene_list
 
 
-def save_cell_graph(gene_path, save_path, type):
-    if os.path.exists(os.path.join(save_path, f'cell_feature_std_{type}.pkl')):
-        print('already exists!')
-    else:
-        # os.makedirs(save_path)
-        exp = pd.read_csv(os.path.join(gene_path, 'CCLE_2369_EXP.csv'), index_col=0)
-        index = exp.index
-        columns = exp.columns
-
-        scaler = StandardScaler()
-        exp = scaler.fit_transform(exp)
-        # cn = scaler.fit_transform(cn)
-        with open(os.path.join(gene_path, 'scaler.pkl'), 'wb') as file:
-            pickle.dump(scaler, file)
-
-        imp_mean = SimpleImputer()
-        exp = imp_mean.fit_transform(exp)
-        with open(os.path.join(gene_path, 'imp_mean.pkl'), 'wb') as file:
-            pickle.dump(imp_mean, file)
-
-        exp = pd.DataFrame(exp, index=index, columns=columns)
-        # cn = pd.DataFrame(cn, index=index, columns=columns)
-        # mu = pd.DataFrame(mu, index=index, columns=columns)
-        cell_names = exp.index
-
-        cell_dict = {}
-
-        for i in tqdm((cell_names)):
-
-            # joint graph (without pathway)
-            if type == 'joint':
-                gene_list = exp.columns.to_list()
-                gene_list = set()
-                for pw in kegg:
-                    for gene in kegg[pw]:
-                        if gene in exp.columns.to_list():
-                            gene_list.add(gene)
-                gene_list = list(gene_list)
-                cell_dict[i] = Data(x=torch.tensor([exp.loc[i, gene_list]], dtype=torch.float).T)
-                # cell_dict[i] = Data(x=torch.tensor([cn.loc[i]], dtype=torch.float).T)
-                # cell_dict[i] = Data(x=torch.tensor([mu.loc[i]], dtype=torch.float).T)
-                # cell_dict[i] = Data(x=torch.tensor([exp.loc[i], cn.loc[i], mu.loc[i]], dtype=torch.float).T)
-                # cell_dict[i] = Data(x=torch.tensor([exp.loc[i], cn.loc[i], mu.loc[i], me.loc[i]], dtype=torch.float).T)
-                # cell_dict[i] = [np.array(exp.loc[i], dtype=np.float32), np.array(cn.loc[i], dtype=np.float32), np.array(mu.loc[i], dtype=np.float32)] # MLP용 코드
-            
-            # disjoint graph (with pathway)
-            else:
-                genes = exp.columns.to_list()
-                x_mask = []
-                x = []
-                gene_list = {}
-                for p, pw in enumerate(list(kegg)):
-                    gene_list[pw] = []
-                    for gene in kegg[pw]:
-                        if gene in genes:
-                            gene_list[pw].append(gene)
-                            x_mask.append(p)
-                    x.append(exp.loc[i, gene_list[pw]])
-                x = pd.concat(x)
-                cell_dict[i] = Data(x=torch.tensor([x], dtype=torch.float).T, x_mask=torch.tensor(x_mask, dtype=torch.int))
-
-        # print(cell_dict)
-        with open(os.path.join(save_path, f'cell_feature_std_{type}.pkl'), 'wb') as file:
-                  pickle.dump(cell_dict, file)
-        print("finish saving cell data!")
-        return gene_list
-
-
-def get_STRING_edges(gene_path, ppi_threshold, type, gene_list):
-    save_path = os.path.join(gene_path, f'edge_index_{ppi_threshold}_{type}.npy')
+def get_STRING_edges(args, gene_list):
+    save_path = os.path.join(args.path_data, f'edge_index_{args.type}.npy')
     if not os.path.exists(save_path):
         # gene_list
-        ppi = pd.read_csv(os.path.join(gene_path, f'CCLE_2369_{ppi_threshold}.csv'), index_col=0)
+        ppi = pd.read_csv(os.path.join(args.ppi), index_col=0)
 
         # joint graph (without pathway)
         if type == 'joint':
@@ -116,7 +115,7 @@ def get_STRING_edges(gene_path, ppi_threshold, type, gene_list):
         # Conserve edge_index
         # print(len(edge_index[0]))
         np.save(
-            os.path.join(rpath + 'Data/Cell/', f'edge_index_{ppi_threshold}_{type}.npy'),
+            save_path,
             edge_index)
     else:
         edge_index = np.load(save_path)
@@ -125,15 +124,7 @@ def get_STRING_edges(gene_path, ppi_threshold, type, gene_list):
 
 
 if __name__ == '__main__':
-    rpath = './'
-    gene_path = rpath+'Data/Cell'
-    save_path = rpath+'Data/Cell'
-    with open(gene_path+'/34pathway_score990.pkl', 'rb') as file:
-        kegg = pickle.load(file)
+    args = parse_args()
 
-  
-    type = 'disjoint'  # type = joint, disjoint, ...
-    
-
-    genelist = save_cell_graph(gene_path, save_path, type=type)
-    get_STRING_edges(gene_path, ppi_threshold='PPI_990', type=type, gene_list=genelist)
+    genelist = save_cell_graph(args)
+    get_STRING_edges(args, gene_list=genelist)
